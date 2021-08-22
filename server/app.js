244eaec9a7506a39425214309fe6e767d2579e9e
Emil Frisk <incrassatum9876@gmail.com>
2021-08-22 13:32:38 +0300

    Added server to the project

---
+++ server/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+app.listen(5000, () => console.log('Listening at 5000'))
+
+// View engine
+app.set('view engine', 'ejs')
+
+// Middleware
+app.use(express.static('public'))
+
+// Routes
+
+app.get('/home', (req, res) => {
+    res.render('index')
+})
